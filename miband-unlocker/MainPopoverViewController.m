317f31a2ddae6d73ca7ae172fd24a9cb769a277f
pangliang <418094911@qq.com>
2015-06-05 18:03:31 +0800

    灵敏度, lock unlock阀值滚动条 可用

--- miband-unlocker/MainPopoverViewController.m
+++ miband-unlocker/MainPopoverViewController.m
@@ -13,10 +13,18 @@
 
 @interface MainPopoverViewController ()
 @property(nonatomic,retain)IBOutlet NSTextField* rssiLabel;
-@property(nonatomic,retain)IBOutlet NSTextField* sensitivityLabel;
+@property(nonatomic,retain)IBOutlet NSTextField* lockSliderValueLabel;
+@property(nonatomic,retain)IBOutlet NSTextField* unlockSliderValueLabel;
+
 @property(nonatomic,retain)IBOutlet NSSlider* sensitivitySlider;
+@property(nonatomic,retain)IBOutlet NSSlider* rssiSlider;
+@property(nonatomic,retain)IBOutlet NSSlider* lockSlider;
+@property(nonatomic,retain)IBOutlet NSSlider* unlockSlider;
 @property MacLocker* macLocker;
 @end
+
+
+int lockUnlockSpace = 10;
 
 @implementation MainPopoverViewController
 
@@ -27,39 +35,72 @@
     return self;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (IBAction)lockSliderMove:(id)sender{
+    float rssiToLock = [self reverseVal:self.lockSlider];
+    float rssiToUnlock = [self reverseVal:self.unlockSlider];
+    
+    if(rssiToLock > rssiToUnlock - lockUnlockSpace )
+    {
+        rssiToLock = rssiToUnlock - lockUnlockSpace;
+        [self setReverseVal:self.lockSlider value:rssiToLock];
+    }
+    
+    [self.lockSliderValueLabel setStringValue: [NSString stringWithFormat:@"%4.1f",rssiToLock]];
 }
 
-- (IBAction)sliderMove:(id)sender{
-    [self.sensitivityLabel setStringValue: [NSString stringWithFormat:@"%d",[self.sensitivitySlider intValue]]];
+- (IBAction)unlockSliderMove:(id)sender{
+    float rssiToLock = [self reverseVal:self.lockSlider];
+    float rssiToUnlock = [self reverseVal:self.unlockSlider];
+    
+    if(rssiToUnlock < rssiToLock + lockUnlockSpace )
+    {
+        rssiToUnlock = rssiToLock + lockUnlockSpace;
+        [self setReverseVal:self.unlockSlider value:rssiToUnlock];
+    }
+    
+    [self.unlockSliderValueLabel setStringValue: [NSString stringWithFormat:@"%4.1f",rssiToUnlock]];
+}
+
+- (float)reverseVal:(NSSlider*) slider{
+    return [slider maxValue] - [slider intValue] + [slider minValue];
+}
+
+- (void)setReverseVal:(NSSlider*)slider value:(float)value{
+    float reverseVal = [slider maxValue] - value + [slider minValue];
+    [slider setFloatValue:reverseVal];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self lockSliderMove:nil];
+    [self unlockSliderMove:nil];
 }
 
 - (void) refreshDeviceRssi:(NSMutableArray *)toProceessDocids{
     while(true){
+        int sensitivity = [self reverseVal:self.sensitivitySlider];
+        
         dispatch_async(dispatch_get_main_queue(), ^{
             if([[BLEDeviceManager getInstance].devices count] > 0)
             {
                 Device* device = (Device*)[[[BLEDeviceManager getInstance].devices allValues] objectAtIndex:0];
                 
-                int sensitivity = 10 - [self.sensitivitySlider intValue] + 1;
-                
                 if( device.refreshRssiTimes >= sensitivity )
                 {
                     float avgRssi =  [device getAvgRssi: sensitivity];
                     [self.rssiLabel setStringValue: [NSString stringWithFormat:@"%4.1f",avgRssi]];
+                    [self setReverseVal:self.rssiSlider value:avgRssi];
                     
-                    if(avgRssi < -83)
+                    if(avgRssi < [self reverseVal:self.lockSlider])
                     {
                         [self.macLocker lock];
-                    }else if(avgRssi > -70){
+                    }else if(avgRssi > [self reverseVal:self.unlockSlider]){
                         [self.macLocker unlock];
                     }
                 }
-                
             }
         });
-        [NSThread sleepForTimeInterval:0.7f];
+        [NSThread sleepForTimeInterval:0.5f+sensitivity/2];
     }
 }
 
